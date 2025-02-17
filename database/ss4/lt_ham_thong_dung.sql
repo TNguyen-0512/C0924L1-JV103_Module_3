b21b1b71a034dcdd9031e7221adac6733874060b
Your Name <btn8176@gmail.com>
2025-02-17 20:13:55 +0700

    baitap

---
+++ database/ss4/lt_ham_thong_dung.sql
@@ -0,0 +1,36 @@
+use quan_ly_sinh_vien;
+
+SELECT 
+    *
+FROM
+    subject
+WHERE
+    credit = (SELECT 
+            MAX(credit)
+        FROM
+            subject);
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+
+SELECT 
+    c.name
+FROM
+    student s
+        JOIN
+    class c ON s.class_id = c.id
+WHERE
+    s.point = (SELECT 
+            MAX(point)
+        FROM
+            student);
+
+-- Hiển thị các thông tin sinh viên (em thay bằng lớp vì mỗi sinh viên chỉ có 1 điểm) và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+
+SELECT 
+    c.name, AVG(point) average_point
+FROM
+    student s
+        JOIN
+    class c ON s.class_id = c.id
+GROUP BY c.name
+ORDER BY average_point DESC;
