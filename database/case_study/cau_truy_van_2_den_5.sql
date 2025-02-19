2831b50a710efb72fdf81effdbc219795ac80100
Your Name <btn8176@gmail.com>
2025-02-19 21:16:51 +0700

    baitap

---
+++ database/case_study/cau_truy_van_2_den_5.sql
@@ -0,0 +1,23 @@
+-- câu 2--
+select *  
+from nhan_vien  
+where (ho_ten like 'H%' OR ho_ten like 'T%' OR ho_ten like 'K%')  
+and char_length(ho_ten) <= 15;
+-- câu 3--
+select *
+from khach_hang
+where timestampdiff(year, ngay_sinh, curdate()) between 18 and 50
+And( dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%');
+-- câu 4--
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, COUNT(hop_dong.ma_hop_dong) as so_lan_dat_phong
+from khach_hang
+join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+where loai_khach.ten_loai_khach = 'Diamond'
+group by khach_hang.ma_khach_hang, khach_hang.ho_ten
+order by so_lan_dat_phong asc;
+-- câu 5 --
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc,
+
+from khach_hang
+join hop_dong on hop_dong.ma_hop_dong,
--- database/m3_casetudy.sql
+++
@@ -1,8 +0,0 @@
--- tạo csdl---
-create database if not exists m3_casestudy;
-use m3_casestudy;
--- tạo table vị trí ---
-create table vi_tri(
-	ma_vi_tri int primary key,
-    ten_vi_tri varchar(50)
-);
